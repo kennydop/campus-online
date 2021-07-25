01dd7e9075861af6ec36c450d06ad205db33cc52
kennydop <dansooffeipatrick@gmail.com>
2021-07-25 21:31:15 +0000

    login

--- pages/Login.js
+++ pages/Login.js
@@ -1,10 +1,19 @@
+import Image from "next/image";
+import campus_online_logo from "../images/campus-online-logo.png"
+
 function Login() {
     return (
         <div className="flex">
-            <div className = "text-pink-500 w-1/2 h-full">
+            <div className = "hidden md:flex text-pink-500 w-1/2 h-full">
             </div>
-            <div>
-                <h1 className="p-3 text-gray-500">Login</h1>
+            <div className="m-auto md:m-0 h-full">
+                <div className="flex items-center justify-center my-auto mx-auto mb-3">
+                        <Image src = {campus_online_logo}
+                        width = {192} 
+                        height = {34.5} 
+                        layout = "fixed"
+                        alt = "campus online logo"/>
+                </div>
                 <form className="flex flex-col">
                         <input className = "infofield" type = "text" placeholder = "Username"/>
                         <input className = "infofield" type = "password" placeholder = "Password"/>
