a7529c60ab56e3520f5b2de6c620d1661b55dcdc
kennydop <dansooffeipatrick@gmail.com>
2021-08-09 15:19:33 +0000

    fixed college duplication

--- pages/add_college.js
+++ pages/add_college.js
@@ -1,25 +1,27 @@
 import {useRouter} from 'next/router';
 import {db, auth, firebaseApp} from '../firebase/firebase';
-import { useState } from 'react';
-import {useSession} from 'next-auth/client';
-import NotAuthorized from '../components/notAuthorized'
+import { useState, useEffect } from 'react';
+import {useSession, getSession} from 'next-auth/client';
+import NotAuthorized from '../components/notAuthorized';
 
-function add_college() {
+function add_college({colleges}) {
     const router = useRouter();
     const [college, setCollege] = useState("");
     const [session, loading] = useSession();
-    db.collection("universities").get().then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-            var collegeOption = document.createElement("option")
-            collegeOption.innerHTML = doc.id
-            document.getElementById('colleges').appendChild(collegeOption)
+
+    function fillColleges(){
+        var alreadyAdded = Array.from(document.querySelectorAll('#colleges option'));
+        colleges.forEach(col => {
+                if(alreadyAdded.length===0){
+                var collegeOption = document.createElement("option");
+                collegeOption.innerHTML = col;
+                document.getElementById('colleges').appendChild(collegeOption)
+            }
         });
-        setCollege('Academic City University College');
-    });
+    }
 
     async function confirmCollege(){
         db.collection("users").doc(auth.currentUser.uid).set({college: college});
-        // db.collection('universities').doc(college).set({registeredUsers: +1})
         const collegeRef = db.collection('universities').doc(college);
         const increment = firebaseApp.firestore.FieldValue.increment(1);
         collegeRef.update({ registeredUsers: increment });
@@ -31,7 +33,7 @@ function add_college() {
         <div className = "h-screen flex flex-col items-center justify-center">
             <div className = "mb-10"><h1 className = "text-lg font-bold text-gray-500">Finish Setting up your Account</h1></div>
             <div className = "bg-gray-100 rounded-full space-between justify-center text-center">
-                <label className = "text-gray-500 mx-4"> Select College:
+                <label className = "text-gray-500 mx-4" onClick={fillColleges}> Select College:
                     <select id = 'colleges' value={college} onChange = {e=>setCollege(e.target.value)} required = "required" className = "bg-gray-100 text-gray-500 h-12 w-40 md:w-60 outline-none pl-4">
                     </select>
                 </label>
@@ -48,5 +50,23 @@ function add_college() {
     </main>
     )
 }
+
+  export async function getStaticProps() {
+    // Call an external API endpoint to get posts
+    var colleges = new Array();
+    await db.collection("universities").get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            colleges.push(doc.id);
+        });
+    });
+    console.log(colleges)
+    // By returning { props: { posts } }, the Blog component
+    // will receive `posts` as a prop at build time
+    return {
+        props: {
+        colleges,
+      },
+    }
+  }
 
 export default add_college
