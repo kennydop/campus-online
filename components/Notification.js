ddf16a944554151c5a30302d29ad53ac53a5f963
Patrick Danso Offei <dansooffeipatrick@gmail.com>
2022-02-07 11:59:22 +0000

    build fix

--- components/Notification.js
+++ components/Notification.js
@@ -68,11 +68,11 @@ function Notification({notification}) {
             <button className={`clicky h-7 w-20 rounded-full shadow-md  text-center cursor-pointer hover:shadow-lg dark:shadow-lg dark:hover:shadow-xl ${from?.followers.indexOf(currentUser._id) > -1 ? 'bg-white dark:bg-bdark-200 text-pink-500 border border-pink-500' : 'bg-pink-500 text-white dark:text-gray-200' }`} onClick={followUser}>{from?.followers.indexOf(currentUser._id) > -1 ? "Unfollow" : "Follow"}</button>
           </div>
           :
-          <Link href={`/p/${notification.post}`}>
+          <Link href={`/p/${notification.post}`} passHref>
             <div className="flex w-full justify-between items-center overflow-hidden cursor-pointer h-full">
               <div className={`${notification.thumbnail ? 'w-10/12' : 'w-full'}`}>
                 <span className="truncate whitespace-pre-wrap break-all w-full">
-                  <Link href={`/u/${from?.username}`}>
+                  <Link href={`/u/${from?.username}`} passHref>
                     <span className="font-semibold">{from?.username}</span>
                   </Link>
                     <span>{` ${notification.message} `}</span>
